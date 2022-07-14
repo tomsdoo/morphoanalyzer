237e091dd3c1d7acf8fe012b2c40ead6f589e4b8
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-07-14 13:41:38 +0900

    dist is ignored (#7)

--- dist/cjs/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare type SimpleToken = {
-    surface: string;
-    pos: string;
-};
-export declare class Analyzer {
-    static analyze(sentence: string): Promise<SimpleToken[]>;
-}
-export default Analyzer;
