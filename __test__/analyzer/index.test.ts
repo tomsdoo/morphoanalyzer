27a5ed12486683ffd0091082ccacb351e855580a
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2024-11-10 12:37:19 +0900

    vitest (#21)

---
+++ __test__/analyzer/index.test.ts
@@ -0,0 +1,27 @@
+import { beforeEach, afterEach, describe, it, expect, vi } from "vitest";
+import { Analyzer, type SimpleToken } from "../../src/analyzer/";
+
+describe("Analyzer", () => {
+  let sentence: string;
+  let expectedResults: Array<SimpleToken>;
+  beforeEach(() => {
+    sentence = "これは、テストです。";
+    expectedResults = [
+      { surface: 'これ', pos: '名詞' },
+      { surface: 'は', pos: '助詞' },
+      { surface: '、', pos: '記号' },
+      { surface: 'テスト', pos: '名詞' },
+      { surface: 'です', pos: '助動詞' },
+      { surface: '。', pos: '記号' }
+    ];
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+  it("analyze()", async () => {
+    await expect(Analyzer.analyze(sentence)).resolves.toEqual(expectedResults);
+  });
+});
+
+
